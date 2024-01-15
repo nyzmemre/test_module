7c9141036f7a8ff50aed3e1ae2cf70334cdb4a98
nyzmemre <nyz_memre_09@hotmail.com>
2024-01-15 23:58:06 +0300

    start animation page design

---
+++ lib/product/animations/transition_page.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+
+class TransitionPage extends StatelessWidget {
+  const TransitionPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
