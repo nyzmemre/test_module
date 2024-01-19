55a0d0e7e6f4274d464a0731322823cc616231ec
nyzmemre <nyz_memre_09@hotmail.com>
2024-01-19 23:54:32 +0300

    add something

---
+++ lib/features/test/simple_test_list_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class SimpleTestList extends StatelessWidget {
+  const SimpleTestList({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
