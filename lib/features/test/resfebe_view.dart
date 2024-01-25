47dad9704d405c70b96b208a9e93fca14e4403f2
nyzmemre <nyz_memre_09@hotmail.com>
2024-01-26 00:02:37 +0300

    added resfebe view

--- lib/features/test/resfebe_view.dart
+++ lib/features/test/resfebe_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:kartal/kartal.dart';
 import 'package:test_module/product/widgets/my_scaffold.dart';
 
@@ -7,20 +8,53 @@ class ResfebeView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    List<TextEditingController> _controllerList=List.generate(3, (index) => TextEditingController());
     return MyScaffold(
       child: Padding(
         padding: context.padding.normal,
         child: Column(
           children: [
             Image.asset('assets/quessImage/19.jpg'),
-            TextFormField(
-              decoration: InputDecoration(
-                labelText: 'Name',
-                border: OutlineInputBorder(),),
+            Expanded(
+              child: ListView.builder(
+                scrollDirection: Axis.horizontal,
+                shrinkWrap: true,
+                  itemCount: _controllerList.length,
+                  itemBuilder: (context, index) {
+                return Row(
+                  children: [
+                    letterTextForm(context, _controllerList[index]),
+                    context.sized.emptySizedWidthBoxLow
+                  ],
+                );
+              }),
             )
           ],
         ),
       ),
     );
+  }
+
+  SizedBox letterTextForm(BuildContext context, TextEditingController controller) {
+    return SizedBox(
+                width: 40,
+                height: 60,
+                child: TextFormField(
+                  controller: controller,
+                  textCapitalization: TextCapitalization.words,
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(5),
+                    )),
+                  onChanged: (value) {
+                    if (value.length == 1) {
+                      FocusScope.of(context).nextFocus(); //whatever you want to do
+                    }
+                  },
+                  inputFormatters: [
+                    LengthLimitingTextInputFormatter(1)
+                  ],
+                ),
+              );
   }
 }
