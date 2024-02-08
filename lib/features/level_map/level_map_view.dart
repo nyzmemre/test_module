233b73321b400e5a05464967943727395063fdaa
nyzmemre <nyz_memre_09@hotmail.com>
2024-02-08 09:11:09 +0300

    trainer with animated level map

--- lib/features/level_map/level_map_view.dart
+++ lib/features/level_map/level_map_view.dart
@@ -13,6 +13,9 @@ class _LevelMapViewState extends State<LevelMapView> {
   final double _mapHeight = 3000.0;
   final double _cameraHeight = 800.0;
   final double _cameraWidth = 800.0;
+  final List<double> _positionLeft=List.generate(10, (index) => index+100);
+  bool _isClick=false;
+  double _position=0;
 
 
   late double _leftPos; //the offset of the widget position relative to the camera
@@ -45,28 +48,90 @@ class _LevelMapViewState extends State<LevelMapView> {
           _leftPos = leftPos;
         });
       },
-      child: Container(
-        height: context.sized.height,
-        width: context.sized.width,
-        decoration: BoxDecoration(
-          color: Colors.white,
-          borderRadius: BorderRadius.circular(15.0),
-          border: Border.all(color: Colors.grey, style: BorderStyle.solid, width: 0.8),
-        ),
-        child: Stack(
-          children: <Widget>[
-            Positioned(
-              left:_leftPos+0,
-              top:_topPos+0,
-              child: Container(
-                width: 30,
-                height: 30,
-                color: Colors.black,
+      child:  Stack(
+        children: <Widget>[
+          Container(
+            height: context.sized.height-56,
+            child: ListView.builder(
+              shrinkWrap: true, // ListView.builder'ın içeriğine sığacak şekilde boyutunu sıkıştır
+              padding: EdgeInsets.all(0),
+              itemCount: 1,
+              scrollDirection: Axis.horizontal,
+              itemBuilder: (context, int index) => Container(
+                margin: EdgeInsets.all(0),
+                padding: EdgeInsets.all(0),
+                height: 600, // ListView.builder'ın yüksekliği 200 piksel olarak ayarlanır
+                width: context.sized.width, //* 15,
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage('assets/appImage/river2_bg.png'),
+                    fit: BoxFit.fill,
+                  ),
+                  color: Colors.white,
+                ),
               ),
             ),
-            Positioned(
-              left:_leftPos+150,
-              top:_topPos+150,
+          ),
+
+          // _positionLeft listesindeki her öğe için bir Positioned widget'ı oluşturun
+          for (int index = 0; index < _positionLeft.length; index++)
+            AnimatedPositioned(
+              duration: Duration(milliseconds: 500),
+              left: _position,
+              top: (context.sized.height-56)*.5,//(context.sized.height-56-context.sized.width*.3)*.5,
+              child: Container(
+                width: context.sized.width*.2,
+                height: context.sized.width*.2,
+                  decoration: BoxDecoration(
+                    image: DecorationImage(image: AssetImage('assets/appImage/ship.png'), fit: BoxFit.fill)
+                  )
+                ),
+              ),
+          Positioned(
+              left:context.sized.width*.05,
+              top: (context.sized.height-56)*.53,
+              child: InkWell(
+                onTap: (){
+                  counter();
+                },
+                child:myContainer(),
+
+
+              ),
+            ),
+          Positioned(
+              left:context.sized.width*.25,
+              top: (context.sized.height-56)*.53,
+              child: InkWell(
+                onTap: (){
+                  counter();
+                },
+                child:myContainer(),
+              ),
+            ),
+          Positioned(
+              left:context.sized.width*.45,
+              top: (context.sized.height-56)*.53,
+              child: InkWell(
+                onTap: (){
+                  counter();
+                },
+                child:myContainer(),
+              ),
+            ),
+          Positioned(
+              left:context.sized.width*.65,
+              top: (context.sized.height-56)*.53,
+              child: InkWell(
+                onTap: (){
+                  counter();
+                },
+                child:myContainer(),
+              ),
+            ),
+            /* Positioned(
+              left:_leftPos+100,
+              top:_topPos+0,
               child: Container(
                 width: 100,
                 height: 100,
@@ -74,8 +139,8 @@ class _LevelMapViewState extends State<LevelMapView> {
               ),
             ),
             Positioned(
-              left:_leftPos+300,
-              top:_topPos+300,
+              left:_leftPos+200,
+              top:_topPos+0,
               child: Container(
                 width: 100,
                 height: 100,
@@ -84,7 +149,7 @@ class _LevelMapViewState extends State<LevelMapView> {
             ),
             Positioned(
               left:_leftPos+450,
-              top:_topPos+450,
+              top:_topPos+0,
               child: Container(
                 width: 100,
                 height: 100,
@@ -93,7 +158,7 @@ class _LevelMapViewState extends State<LevelMapView> {
             ),
             Positioned(
               left:_leftPos+700,
-              top:_topPos+700,
+              top:_topPos+0,
               child: Container(
                 width: 100,
                 height: 100,
@@ -102,16 +167,15 @@ class _LevelMapViewState extends State<LevelMapView> {
             ),
             Positioned(
               left:_leftPos+900,
-              top:_topPos+900,
+              top:_topPos+0,
               child: Container(
                 width: 100,
                 height: 100,
                 color: Colors.blueAccent,
               ),
-            )
+            )*/
           ],
         ),
-      ),
     );
 
     return Scaffold(
@@ -119,6 +183,7 @@ class _LevelMapViewState extends State<LevelMapView> {
         // Here we take the value from the MyHomePage object that was created by
         // the App.build method, and use it to set our appbar title.
         title: Text('widget.title'),
+
       ),
       body: Center(
         // Center is a layout widget. It takes a single child and positions it
@@ -126,6 +191,43 @@ class _LevelMapViewState extends State<LevelMapView> {
         child: camera,
       ),
     );
+  }
+
+  Container myContainer() {
+    return Container(
+                width: 35,
+                height: 35,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(50), // Yuvarlak kenarlar
+                  gradient: LinearGradient(
+                    colors: [
+                      Color(0xFFE74C3C), // Koyu kırmızı tonu
+                      Color(0xFFC0392B), // Orta tonlu kırmızı
+                      Color(0xFF922B21), // Daha açık kırmızı tonu
+                    ],
+                    begin: Alignment.topLeft,
+                    end: Alignment.bottomRight,
+                  ),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.black.withOpacity(0.5),
+                      spreadRadius: 5,
+                      blurRadius: 7,
+                      offset: Offset(0, 3), // Gölgelendirme konumu
+                    ),
+                  ],
+                ),
+                child: Center(
+                  child: Text(
+                    '1',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              );
   }
   double _boundaryRule(position, mapLength, cameraLength){
     // this function will prevent the widget from moving if it reached the boundary
@@ -137,5 +239,12 @@ class _LevelMapViewState extends State<LevelMapView> {
     }*/
 
     return position;
+  }
+
+  void counter(){
+_position+=context.sized.width*.2;
+setState(() {
+
+});
   }
 }
