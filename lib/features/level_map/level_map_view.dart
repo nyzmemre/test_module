8e4ba25094e4ab95993560bd18bcbf6ddd29f6ee
nyzmemre <nyz_memre_09@hotmail.com>
2024-02-10 08:36:08 +0300

    code ui refactoring for level map view

--- lib/features/level_map/level_map_view.dart
+++ lib/features/level_map/level_map_view.dart
@@ -13,57 +13,73 @@ class LevelMapView extends StatelessWidget {
     return SafeArea(
         child: Scaffold(
 
-      body: Container(
-        width: context.sized.width,
-        height: context.sized.height,
-        padding: context.padding.normal,
-        alignment: Alignment.center,
-        decoration: const BoxDecoration(
-            image: DecorationImage(
-                image: AssetImage(
-                  ImagePaths.levelBackgroundImagePath,
-                ),
-                fit: BoxFit.cover),
-        color: Colors.blue),
-        child: Column(
-          children: [
-
-            Expanded(
-              child: Container(
-                //width: context.sized.width * .9,
-                //height: context.sized.height* .4,
-                padding: context.padding.low,
-                //alignment: Alignment.center,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(30),
-                  color: Colors.white.withOpacity(.6),
-              
-                ),
-                child: GridView.builder(
-                  shrinkWrap: true,
-                    physics: ScrollPhysics(),
-                    gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                      crossAxisCount: 4,
-                      childAspectRatio: 1.0
-              
-                    ),
-                    itemCount: 44,
-                    itemBuilder: (context, int index) {
-                      return levelBgContainer(context, _isLock, index);
-                    }),
-              ),
-            ),
-            Container(
-              width: context.sized.width*.5,
-              height: context.sized.height*.1,
-              decoration: BoxDecoration(
-                  image: DecorationImage(image: AssetImage(ImagePaths.rectButtonImagePath))
-              ),
-            ),
-          ],
-        ),
-      ),
+      body: level_map_background_cont(context, _isLock),
     ));
+  }
+
+  Container level_map_background_cont(BuildContext context, List<bool> _isLock) {
+    return Container(
+      width: context.sized.width,
+      height: context.sized.height,
+      padding: context.padding.normal,
+      alignment: Alignment.center,
+      decoration: const BoxDecoration(
+          image: DecorationImage(
+              image: AssetImage(
+                ImagePaths.levelBackgroundImagePath,
+              ),
+              fit: BoxFit.cover),
+      color: Colors.blue),
+      child: Column(
+        children: [
+
+          grid_container(context, _isLock),
+          start_button(context),
+        ],
+      ),
+    );
+  }
+
+  Expanded grid_container(BuildContext context, List<bool> _isLock) {
+    return Expanded(
+            child: Container(
+              //width: context.sized.width * .9,
+              //height: context.sized.height* .4,
+              padding: context.padding.low,
+              //alignment: Alignment.center,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(30),
+                color: Colors.white.withOpacity(.6),
+
+              ),
+              child: my_gridview(_isLock),
+            ),
+          );
+  }
+
+  GridView my_gridview(List<bool> _isLock) {
+    return GridView.builder(
+                shrinkWrap: true,
+                  physics: ScrollPhysics(),
+                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 4,
+                    childAspectRatio: 1.0
+
+                  ),
+                  itemCount: 44,
+                  itemBuilder: (context, int index) {
+                    return levelBgContainer(context, _isLock, index);
+                  });
+  }
+
+  Container start_button(BuildContext context) {
+    return Container(
+            width: context.sized.width*.5,
+            height: context.sized.height*.1,
+            decoration: BoxDecoration(
+                image: DecorationImage(image: AssetImage(ImagePaths.rectButtonImagePath), )
+            ),
+          );
   }
 
   Widget levelBgContainer(BuildContext context, List<bool> _isLock, int index) {
